657ebdeda1b0d3d1b69e1e9766716847e667d36f
Vitor Melo <vtmx22@gmail.com>
2017-08-08 11:25:51 -0300

    Add titletag in functions.php

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
 function admin_favicon() {
 	echo '<link rel="shortcut icon" href="'. get_template_directory_uri() .'/favicon-admin.png">';
 }
-add_action('admin_head', 'admin_favicon'); 
+add_action('admin_head', 'admin_favicon');
 
 
 
@@ -29,5 +29,8 @@ function register_my_menu() {
   register_nav_menu('header-menu', 'Menu Cabeçalho');
 }
 add_action( 'init', 'register_my_menu' );
+
+
+add_theme_support( 'title-tag' );
 
 ?>
